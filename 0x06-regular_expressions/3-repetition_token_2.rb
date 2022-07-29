ff4c2e45458a6d162b80d7d8d2a72862a0d2afb4
Elfueros <odidiperez@gmail.com>
2022-07-29 20:53:12 +0000

    reviews

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-# Must match "hbtn, hbttn, hbtttn, hbttttn" but not "hbn"
+# Must match "hbtn, hbttn, hbtttn, hbttttn" but not "hbn" any number of t
 
 puts ARGV[0].scan(/hbt+n/).join
